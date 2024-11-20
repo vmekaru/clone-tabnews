35767b8eff15aad7e5eaff0d1e57641e265ab4a9
Vitor Mekaru <87271447+vmekaru@users.noreply.github.com>
2024-11-20 18:09:10 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Raíssa, eu te amo. Obrigado por fazer parte da minha vidinha s2</h1>
+function Home() {
+  return (
+    <h1>Raíssa, eu te amo. Obrigado por fazer parte da minha vidinha s2</h1>
+  );
 }
 
 export default Home;
