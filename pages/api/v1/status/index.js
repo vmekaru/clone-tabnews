95fa2b7914afee8c1492c9c4e44fdafc486aa9c3
Vitor Mekaru <87271447+vmekaru@users.noreply.github.com>
2025-09-11 12:20:58 +0000

    add `database.js`and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT NOW()');
+  console.log(result)
   response.status(200).json({
     status: 'ok',
     timestamp: new Date().toISOString(),
